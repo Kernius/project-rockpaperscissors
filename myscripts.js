e802e4dc6286af24bf4ca2a2d9bd493a8770755f
Kernius Kaladinskas <kernius1@gmail.com>
2022-11-10 20:04:55 -0500

    Fixed getting playerSelection by adding getElementById to playRound conditionals

--- myscripts.js
+++ myscripts.js
@@ -4,17 +4,17 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection,computerSelection) {
-    if (playerSelection === "<button>Rock</button>" && computerSelection === "Paper") {
+    if (playerSelection === document.getElementById("Rock") && computerSelection === "Paper") {
         return "Computer chose Paper! You lost the round"
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    } else if (playerSelection === document.getElementById("Scissors") && computerSelection === "Rock") {
         return "Computer chose Rock! You lost the round"
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    } else if (playerSelection === document.getElementById("Paper") && computerSelection === "Scissors") {
         return "Computer chose Scissors! You lost the round"
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    } else if (playerSelection === document.getElementById("Rock") && computerSelection === "Scissors") {
         return "Computer chose Scissors! You win the round!"
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection === document.getElementById("Scissors") && computerSelection === "Paper") {
         return "Computer chose Paper! You win the round!"
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    } else if (playerSelection === document.getElementById("Paper") && computerSelection === "Rock") {
         return "Computer chose Rock! You win the round!"
     } else {
         return "Draw!"
@@ -36,11 +36,12 @@ button.addEventListener('click', function (e)  {
         const result = playRound(playerSelection,computerSelection);
             if(result.includes("You win") ) {
                 playerPoints++;
-            } else if (result.includes("next time") ) {
+            } else if (result.includes("You lost") ) {
                 computerPoints++;
             }
-            console.log(computerSelection);
-            console.log(playerSelection);
+            console.log("Your score:" + playerPoints);
+                console.log("Computers score:" + computerPoints);
+                console.log(playRound(playerSelection,computerSelection))
     }
  });
 });
